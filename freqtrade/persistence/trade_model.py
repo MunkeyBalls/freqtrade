257a2d0b8a77ea6aa1c726b172dc434491215378
munkey <you@example.com>
2022-10-15 18:12:16 +0200

    typo

--- freqtrade/persistence/trade_model.py
+++ freqtrade/persistence/trade_model.py
@@ -101,7 +101,7 @@ class Order(_DECL_BASE):
 
         return (f'Order(id={self.id}, order_id={self.order_id}, trade_id={self.ft_trade_id}, '
                 f'side={self.side}, order_type={self.order_type}, status={self.status},' 
-                f'price={self.price}), remaining={self.remaining}')
+                f'price={self.price}, remaining={self.remaining}')
 
     def update_from_ccxt_object(self, order):
         """
